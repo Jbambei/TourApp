cd10d73da0439499ebfef202f059a4addb016ace
Lilu <KHussell@gmail.com>
2019-04-28 19:11:32 -0400

    Layouts for front page, fast pass inuput modal, creat schedule, schedule

---
+++ assets/javascript/kristin.js
@@ -0,0 +1,27 @@
+//fast pass modal opening
+$(document).ready(function(){
+
+
+
+
+    //initialize all modals           
+    $('.modal').modal();
+
+    //now you can open modal from code
+    $('#modal1').modal('open');
+    $('.modal-trigger').modal();
+
+
+    //nav bar drop down 
+    $('.sidenav').sidenav();
+
+
+    //current location dropdown
+    $('select').formSelect();
+
+
+     
+
+     
+
+  });
